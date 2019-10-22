f73e7938af766d0c48b9767e31c6691825bd6710
peach-weiyl <nbzdds08@163.com>
2019-10-22 20:56:43 +0800

    添加手动读码

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ from time import sleep
 from PySide2.QtWidgets import QMainWindow, QApplication, QMessageBox, QSplashScreen, QLabel
 from PySide2.QtCore import QThread, Signal, Qt
 from PySide2.QtGui import QPixmap, QFont
+
+from PIL import ImageGrab
+import pytesseract
+import keyboard
+import pyautogui as pag
 # 自己的包
 from UI2PY.MainWindow import Ui_MainWindow
 from mycode.HslCommunication import SiemensPLCS, SiemensS7Net
@@ -43,25 +48,21 @@ class MyWindow(QMainWindow):
         self.setup()
 
     def setup(self):
-        ip_plc = self.conf.read_config(product='config', section='plc', name='ip')
-        self.Ui_MainWindow.lineEdit_IP_PLC.setText(ip_plc)
-        self.Ui_MainWindow.label_status.setText('正在连接PLC...')
-        self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 255, 127);')
-        QApplication.processEvents()
-        if self.siemens.ConnectServer().IsSuccess:  # 如果连接成功
-            self.Ui_MainWindow.label_status.setText('等待读取钥匙号')
-            self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 255, 127);')
-        else:
-            self.Ui_MainWindow.label_status.setText('PLC连接失败！')
-            self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 0, 0);')
+        pass
 
-        self._thread.start()
-
+    # 槽函数
     def change_ip_plc(self):
+        self._thread.__del__()
+        self._thread.quit()
         ip_plc = self.Ui_MainWindow.lineEdit_IP_PLC.text()
         self.conf.update_config(product='config', section='plc', name='ip', value=ip_plc)
 
     def test_connect_plc(self):
+        self._thread.__del__()
+        self._thread.quit()
+        ip_plc = self.conf.read_config(product=self.product, section='plc', name='ip')
+        self.Ui_MainWindow.lineEdit_IP_PLC.setText(ip_plc)
+        self.siemens = SiemensS7Net(SiemensPLCS.S1200, ip_plc)
         self.Ui_MainWindow.label_status.setText('正在连接PLC...')
         self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 255, 127);')
         QApplication.processEvents()
@@ -99,23 +100,69 @@ class MyWindow(QMainWindow):
         print(marble_is_ready)
         print(self.key_is_ready, self.marble_machine_is_ready)
         if self.key_is_ready and self.marble_machine_is_ready:  # 如果钥匙和弹子机都到位，则读取钥匙号
+            self.Ui_MainWindow.label_status.setText('等待读取钥匙号')
+            self.Ui_MainWindow.label_status.setStyleSheet("background-color: rgb(255, 255, 127);")
+            QApplication.processEvents()
             self.key_is_ready = False
             self.marble_machine_is_ready = False
-            self.product = '281B'
-            keyid = self.get_keyid()
-            res, keycode = self.get_keycode(keyid)
-            self.Ui_MainWindow.lineEdit.setText(keycode)
-            if res:  # 如果正确获取钥匙号
-                self.Ui_MainWindow.label_status.setText('等待读取钥匙号')
-                self.Ui_MainWindow.label_status.setStyleSheet("background-color: rgb(255, 255, 127);")
-                print('打印钥匙号')
-            else:
-                self.Ui_MainWindow.label_status.setText('未正确获取钥匙号')
-                self.Ui_MainWindow.label_status.setStyleSheet("background-color: rgb(255, 0, 0);")
-                print('未正确获取钥匙号')
-            print(keyid, keycode)
+            self.key_read()
 
+    def manual_key_read(self):
+        self.key_read()
+
+    def start(self):
+        ip_plc = self.conf.read_config(product='config', section='plc', name='ip')
+        self.Ui_MainWindow.lineEdit_IP_PLC.setText(ip_plc)
+        self.Ui_MainWindow.label_status.setText('正在连接PLC...')
+        self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 255, 127);')
+        QApplication.processEvents()
+        if self.siemens.ConnectServer().IsSuccess:  # 如果连接成功
+            self.Ui_MainWindow.label_status.setText('等待读取钥匙号')
+            self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 255, 127);')
+            self._thread.start()
+        else:
+            self.Ui_MainWindow.label_status.setText('PLC连接失败！')
+            self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 0, 0);')
+
+    def pause(self):
+        self._thread.__del__()
+        self._thread.quit()
+
+    # 选择图片的识别区域
+    def select_capture_region(self):
+        self._thread.__del__()
+        self._thread.quit()
+        conf = Config()
+        if keyboard.wait(hotkey='ctrl+alt') == None:
+            x1, y1 = pag.position()
+            print(pag.position())
+            if keyboard.wait(hotkey='ctrl+alt') == None:
+                x2, y2 = pag.position()
+                print(pag.position())
+        pos_new = str((x1, y1, x2, y2))
+        conf.update_config(section='image_read_area', name='position', value=pos_new)
+        self.ui.scan_le.setFocus()
+
+    def show_capture(self):
+        self._thread.__del__()
+        self._thread.quit()
+        img, project = self.get_project()
+        img.show()
+
+    # 功能函数
     # 获取弹子号
+    def key_read(self):
+        self.product = '281B'
+        keyid = self.get_keyid()
+        res, keycode = self.get_keycode(keyid)
+        self.Ui_MainWindow.lineEdit.setText(keycode)
+        if res:  # 如果正确获取钥匙号
+            print('打印钥匙号')
+        else:
+            self.Ui_MainWindow.label_status.setText('未正确获取钥匙号')
+            self.Ui_MainWindow.label_status.setStyleSheet("background-color: rgb(255, 0, 0);")
+            print('未正确获取钥匙号')
+
     def get_keyid(self):
         keyid = ''
         read = self.siemens.Read('DB3.60', 32)  # 读取8个弹子号格式：弹子号为11111111，则读取为00010001000100010001000100010001
@@ -138,6 +185,16 @@ class MyWindow(QMainWindow):
             self.Ui_MainWindow.label_status.setText('get_keycode:%s' % str(e))
             self.Ui_MainWindow.label_status.setStyleSheet('background-color: rgb(255, 0, 0);')
             return False, '----'
+
+    def get_project(self):
+        conf = Config()
+        pos = conf.read_config(section="read_area", name="position")
+        img = ImageGrab.grab(pos)
+        # img.show()
+        # img.save("picture2string.jpg")
+        project = pytesseract.image_to_string(img, lang='chi_sim')
+        print(project)
+        return img, project
 
 
 class MyThread(QThread):
